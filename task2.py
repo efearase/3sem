2504a97313ea3d38fab9ca6bac61a2e16047a766
efearase <75952778+efearase@users.noreply.github.com>
2020-12-15 14:26:28 +0300

    Update task2.py
    fixed 2

--- task2.py
+++ task2.py
@@ -3,10 +3,20 @@ import sys
 
 if len(sys.argv) != 3:
     print('Неверное количество аргументов')
-    sys.exit(-2)
+    sys.exit(1)
 
 path = sys.argv[1]
 text = sys.argv[2]
 
-with open(path,'wb') as file:
-    file.write(text.encode('utf-8'))
+elif not os.path.isfile(path):
+    print('Ошибка файла')
+    sys.exit(2)
+
+written_len = 0
+len_to_write = len(text)
+seek = 0
+with open(path, 'rb') as f:
+    while (len_to_write - written_len > 4096):
+        f.write(text[seek:seek+4096])
+        written_len += 4096
+    to_.write(text[seek: len(text-1)])
